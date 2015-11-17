10e225f72ece42e58f55b51111610f7834e3a474
Kanwei Li <kanwei@gmail.com>
2015-11-17 16:30:21 -0500

    Update all dependencies

--- project.clj
+++ project.clj
@@ -3,16 +3,14 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0-alpha5"]
-                 [org.clojure/clojurescript "0.0-2985"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "1.7.170"]
+                 [org.clojure/core.async "0.2.374"]
                  [criterium "0.4.3"]
-                 [reagent "0.4.3"]
-                 [com.taoensso/timbre "3.3.1"]
+                 [reagent "0.5.1"]
+                 [com.taoensso/timbre "4.1.4"]
                  [org.clojure/math.numeric-tower "0.0.4"]]
-  :plugins [[lein-cljsbuild "1.0.5"]]
-  :clean-targets ^{:protect false}
-  ["resources/public/js/cljs.js"]
+  :plugins [[lein-cljsbuild "1.1.1"]]
   :cljsbuild {
     :builds [{:id "ulam"
               :source-paths ["src"]
